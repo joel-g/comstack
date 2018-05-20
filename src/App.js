4737b5d514cbc47a3c38c32e11ce1516effefe27
joel-g <joelaguerra1@gmail.com>
2018-05-19 21:05:15 -0700

    completes api call

--- src/App.js
+++ src/App.js
@@ -4,13 +4,22 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      stackIds: ['8826629','9541448', '6591675', '7452904', '9393813', '8840926', '9454112', '7287446', '9598801', '9541439', '9554961', '8797362', '8748848', '9672092', '9603809', '9536851', '8366814', '9541510', '9541342']
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome to ComStack</h1>
         </header>
-        <Usercard />
+        {
+          this.state.stackIds.map(user => <Usercard user_id={user} />)
+        }
       </div>
     );
   }
--- src/components/Usercard.js
+++ src/components/Usercard.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
 
 class Usercard extends Component {
+    constructor() {
+      super();
+      this.state = {
+        profile_image: null,
+        link: null,
+
+      }
+    }
+
+    componentDidMount() {
+      fetch("http://api.stackexchange.com/2.2/users/" + this.props.user_id + "?order=desc&sort=reputation&site=stackoverflow").then(results => {
+        return results.json();
+      }).then(data => {
+        let user = data.items[0]
+        console.log(data.items[0])
+        this.setState({
+          display_name: user.display_name,
+        })
+      })
+    }
+
     render() {
       return (
         <div className="User">
           <b>{this.props.index}}</b>         
                 <ul class='list'>
-                    <li><img src={this.props.profile_image} class='profile-pic'/></li>
-                    <li class='name'><b><a href={this.props.link}>{{ user.display_name }}</a></b></li>
+                    <li><img src={this.props.profile_image} alt='user profile' class='profile-pic'/></li>
+                    <li class='name'><b><a href={this.props.link}>{this.props.display_name}</a></b></li>
                     <li>Total Rep: {this.props.reputation}</li>
                     <li>Change this week: {this.props.reputation_change_week}</li>
                     <li>Change this month: {this.props.reputation_change_month}</li>
@@ -15,7 +36,7 @@ class Usercard extends Component {
                         <div class='medals'>
                             <span class='gold'>{this.props.badge_counts_gold} ⏺</span>
                             <span class='silver'>{this.props.badge_counts_silver} ⏺</span>
-                            <span class='bronze'>{this.props.badge_counts_bronze } ⏺</span>
+                            <span class='bronze'>{this.props.badge_counts_bronze} ⏺</span>
                         </div>
                     </li>
                 </ul>
