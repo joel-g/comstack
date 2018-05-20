7e02afbb49fbb2baeca9db51ade2d36bc3cb9874
joel-g <joelaguerra1@gmail.com>
2018-05-19 20:16:26 -0700

    initial commit

---
+++ src/components/Usercard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Usercard extends Component {
+    render() {
+      return (
+        <div className="User">
+          {"This is a user"}
+        </div>
+      );
+    }
+  }
+
+  export default Usercard;
