5bc270c94f3a150dbc0106eab3efc70d85fbb71d
joel-g <joelaguerra1@gmail.com>
2018-05-19 20:37:55 -0700

    fills out user card

--- src/components/Usercard.js
+++ src/components/Usercard.js
@@ -4,7 +4,21 @@ class Usercard extends Component {
     render() {
       return (
         <div className="User">
-          {"This is a user"}
+          <b>{this.props.index}}</b>         
+                <ul class='list'>
+                    <li><img src={this.props.profile_image} class='profile-pic'/></li>
+                    <li class='name'><b><a href={this.props.link}>{{ user.display_name }}</a></b></li>
+                    <li>Total Rep: {this.props.reputation}</li>
+                    <li>Change this week: {this.props.reputation_change_week}</li>
+                    <li>Change this month: {this.props.reputation_change_month}</li>
+                    <li>
+                        <div class='medals'>
+                            <span class='gold'>{this.props.badge_counts_gold} ⏺</span>
+                            <span class='silver'>{this.props.badge_counts_silver} ⏺</span>
+                            <span class='bronze'>{this.props.badge_counts_bronze } ⏺</span>
+                        </div>
+                    </li>
+                </ul>
         </div>
       );
     }
